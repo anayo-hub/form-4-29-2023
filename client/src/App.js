1a6ca29c5016c6b6012b46a1366e795f252f4c91
Criss Ayo <anayocrescent2@gmail.com>
2023-04-29 16:54:08 +0100

    add formik and react-toast

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,54 @@
-function App() {
-  return (
-    <div>
-      hello
-    </div>
-  );
-}
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-export default App;
+
+/** import all components */
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+
+
+/** root routes */
+const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <Username></Username>
+    },
+    {
+        path : '/register',
+        element : <Register></Register>
+    },
+    {
+        path : '/password',
+        element : <Password></Password>
+    },
+    {
+        path : '/profile',
+        element : <Profile></Profile>
+    },
+    {
+        path : '/recovery',
+        element : <Recovery></Recovery>
+    },
+    {
+        path : '/reset',
+        element : <Reset></Reset>
+    },
+    {
+        path : '*',
+        element : <PageNotFound></PageNotFound>
+    },
+])
+
+export default function App() {
+  return (
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
+  )
+}
