e2a74e9cc60e5622795950638e2e285f207d3d73
Criss Ayo <anayocrescent2@gmail.com>
2023-04-30 14:32:58 +0100

    finish front end

--- client/src/components/PageNotFound.js
+++ client/src/components/PageNotFound.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import { Link} from 'react-router-dom'
+
 export default function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <div className="container h-screen">
+
+    <div className='flex justify-center items-center h-screen'>
+
+        <div className="title flex flex-col items-center">
+          <h4 className='text-5xl font-bold'>Feeling Lost</h4>
+          <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+             Please check your url or go back to homepage
+          </span>
+          This Page is not Avialble
+                <div className="text-center py-4">
+                <span className='text-gray-500'>Home<Link className='text-red-500' to="/"> Now</Link></span>
+      </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -1,26 +1,21 @@
 import toast from 'react-hot-toast'
+// import { authenticate } from './helper'
 
-// ************************username*****************************//
+/** validate login page username */
 export async function usernameValidate(values){
     const errors = usernameVerify({}, values);
 
+    // if(values.username){
+    //     // check user exist or not
+    //     const { status } = await authenticate(values.username);
+
+    //     if(status !== 200){
+    //         errors.exist = toast.error('User does not exist...!')
+    //     }
+    // }
+
     return errors;
 }
-
-/** validate username */
-function usernameVerify(error = {}, values){
-    if(!values.username){
-        error.username = toast.error('Username Required...!');
-    }else if(values.username.includes(" ")){
-        error.username = toast.error('Invalid Username...!')
-    }
-
-    return error;
-}
-
-// ************************username*****************************//
-
-// ************************password*****************************//
 
 /** validate password */
 export async function passwordValidate(values){
@@ -28,6 +23,35 @@ export async function passwordValidate(values){
 
     return errors;
 }
+
+/** validate reset password */
+export async function resetPasswordValidation(values){
+    const errors = passwordVerify({}, values);
+
+    if(values.password !== values.confirm_pwd){
+        errors.exist = toast.error("Password not match...!");
+    }
+
+    return errors;
+}
+
+/** validate register form */
+export async function registerValidation(values){
+    const errors = usernameVerify({}, values);
+    passwordVerify(errors, values);
+    emailVerify(errors, values);
+
+    return errors;
+}
+
+/** validate profile page */
+export async function profileValidation(values){
+    const errors = emailVerify({}, values);
+    return errors;
+}
+
+
+/** ************************************************* */
 
 /** validate password */
 function passwordVerify(errors = {}, values){
@@ -48,8 +72,26 @@ function passwordVerify(errors = {}, values){
 }
 
 
-// ************************password*****************************//
+/** validate username */
+function usernameVerify(error = {}, values){
+    if(!values.username){
+        error.username = toast.error('Username Required...!');
+    }else if(values.username.includes(" ")){
+        error.username = toast.error('Invalid Username...!')
+    }
 
-// ************************email*****************************//
+    return error;
+}
 
-// // ************************email*****************************//
+/** validate email */
+function emailVerify(error ={}, values){
+    if(!values.email){
+        error.email = toast.error("Email Required...!");
+    }else if(values.email.includes(" ")){
+        error.email = toast.error("Wrong Email...!")
+    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        error.email = toast.error("Invalid email address...!")
+    }
+
+    return error;
+}
