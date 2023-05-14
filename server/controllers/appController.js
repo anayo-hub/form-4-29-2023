eb9a9c82520a7b3234aacbfa5a79b83494bacde5
Criss Ayo <anayocrescent2@gmail.com>
2023-05-14 01:05:37 +0100

    complete routes

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -2,9 +2,7 @@ import UserModel from '../model/User.model.js'
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import ENV from '../config.js'
-// import otpGenerator from 'otp-generator';
-
-/** middleware for verify user */
+import otpGenerator from 'otp-generator';
 
 /** middleware for verify user */
 export async function verifyUser(req, res, next){
@@ -75,9 +73,6 @@ export async function register(req, res) {
     }
 }
 
-
-
-
 /** POST: http://localhost:8080/api/login
  * @param: {
   "username" : "example123",
@@ -141,8 +136,6 @@ export async function getUser(req, res) {
     }
   }
 
-
-
 /** PUT: http://localhost:8080/api/updateuser
  * @param: {
   "header" : "<token>"
@@ -155,15 +148,19 @@ body: {
 */
 export async function updateUser(req, res) {
   try {
-    const id = req.query.id;
 
-    if (!id) {
+     // const id = req.query.id;
+     const { userId } = req.user;
+
+    if (!userId) {
       return res.status(401).send({ error: "User Not Found" });
     }
 
     const userData = req.body;
 
-    await UserModel.updateOne({ _id: id }, userData);
+    console.log(userData);
+
+    await UserModel.updateOne({ _id: userId }, userData);
 
     return res.status(200).send({ message: "User data updated successfully" });
   } catch (error) {
@@ -174,31 +171,75 @@ export async function updateUser(req, res) {
 
 
 
-
-
 /** GET: http://localhost:8080/api/generateOTP */
 export async function generateOTP(req,res){
-    res.json("hello")
+  req.app.locals.OTP = await otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false})
+  res.status(201).send({ code: req.app.locals.OTP })
 }
 
 
 /** GET: http://localhost:8080/api/verifyOTP */
 export async function verifyOTP(req,res){
-    res.json("hello")
+  const { code } = req.query;
+  if(parseInt(req.app.locals.OTP) === parseInt(code)){
+      req.app.locals.OTP = null; // reset the OTP value
+      req.app.locals.resetSession = true; // start session for reset password
+      return res.status(201).send({ msg: 'Verify Successsfully!'})
+  }
+  return res.status(400).send({ error: "Invalid OTP"});
 }
 
 
 // successfully redirect user when OTP is valid
 /** GET: http://localhost:8080/api/createResetSession */
 export async function createResetSession(req,res){
-    res.json("hello")
+  if(req.app.locals.resetSession){
+       return res.status(201).send({ flag : req.app.locals.resetSession})
+  }
+  return res.status(404).send({error : "Session expired!"})
 }
 
 
 // update the password when we have valid session
 /** PUT: http://localhost:8080/api/resetPassword */
-export async function resetPassword(req,res){
-    res.json("hello")
+export async function resetPassword(req, res) {
+  try {
+    // Check if the resetSession flag is set in the app locals.
+    // If it's not set, return a 401 status code and an error message.
+    if (!req.app.locals.resetSession) {
+      return res.status(401).send({ error: "Session expired!" });
+    }
+
+    // Extract the username and password from the request body.
+    const { username, password } = req.body;
+
+    // Find the user in the database based on the username.
+    const user = await UserModel.findOne({ username });
+
+    // If the user is not found, return a 404 status code and an error message.
+    if (!user) {
+      return res.status(404).send({ error: "Username not found" });
+    }
+
+    // Hash the new password using bcrypt.
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Update the user's password in the database.
+    await UserModel.updateOne(
+      { username: user.username },
+      { password: hashedPassword }
+    );
+
+    // Reset the resetSession flag to false.
+    req.app.locals.resetSession = false;
+
+    // Return a 201 status code and a success message.
+    return res.status(201).send({ msg: "Record Updated...!" });
+  } catch (error) {
+    // If there's an error, log it to the console and return a 500 status code and an error message.
+    console.error(error);
+    return res.status(500).send({ error: "Internal server error" });
+  }
 }
 
 
