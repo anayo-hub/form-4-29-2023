d72b79387bbce34306ee967c4b2faaf08ceed343
Criss Ayo <anayocrescent2@gmail.com>
2023-08-03 02:53:00 +0100

    connect mongodb atlas

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 import { MongoMemoryServer } from "mongodb-memory-server";
-// import ENV from '../config.js'
+import ENV from '../config.js'
 
 async function connect(){
 
@@ -10,8 +10,8 @@ async function connect(){
 
     mongoose.set('strictQuery', true)
     // const db = await mongoose.connect(getUri);
-    // const db = await mongoose.connect(ENV.ATLAS_URI);
-    const db = await mongoose.connect(getUri);
+    const db = await mongoose.connect(ENV.ATLAS_URI);
+    // const db = await mongoose.connect(getUri);
     console.log("Database Connected")
     return db;
 }
